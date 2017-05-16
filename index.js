3989a4da26ba9754e15cc3428f4b18694a8e876d
loughrao <loughrao@oregonstate.edu>
2017-05-15 23:03:08 -0700

    Final Commit to index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var modalCloseButton = document.getElementsByClassName("modal-close-button")[0];
 var modalAcceptButton = document.getElementsByClassName("modal-accept-button")[0];
 var twitText = document.getElementById("twit-text-input");
 var twitAuthor = document.getElementById("twit-attribution-input");
+var twitSearch = document.getElementById("navbar-search-input");
+var twitSearchButton = document.getElementById("navbar-search-button")
 
 function modalToggle(event) {
   if (modal.classList.contains("hidden")) {
@@ -30,7 +32,8 @@ function addTwit(event) {
   }
 
   var icon = document.createElement('i');
-  icon.classList.add('fa.fa-bullhorn');
+  icon.classList.add('fa');
+  icon.classList.add('fa-bullhorn');
 
   var twitIcon = document.createElement('div');
   twitIcon.classList.add('twit-icon');
@@ -38,11 +41,11 @@ function addTwit(event) {
 
   var text = document.createElement('p');
   text.classList.add('twit-text');
-  text.value = twitText.value;
+  text.textContent = twitText.value;
 
   var author = document.createElement('a');
   author.href = '#';
-  author.value = twitAuthor.value;
+  author.textContent = twitAuthor.value;
 
   var attribution = document.createElement('p');
   attribution.classList.add('twit-attribution');
@@ -58,12 +61,42 @@ function addTwit(event) {
   twit.classList.add('twit');
   twit.appendChild(twitIcon);
   twit.appendChild(twitContent);
+  
+  var body = document.getElementsByClassName('twit-container')[0];
+  body.appendChild(twit);
 
   modalToggle();
-  return twit;
 }
+
+function search(event) {
+	var twits = document.getElementsByClassName('twit');
+	for (i = 0; i < 8; i++) {
+		if ((twits[i].childNodes[3].childNodes[1].textContent.includes(twitSearch.value)) || (twits[i].childNodes[3].childNodes[3].textContent.includes(twitSearch.value))) {
+			twits[i].classList.remove('hidden');
+			continue;
+		}
+		else {
+			twits[i].classList.add('hidden');
+		}
+	}
+	if (twits.length > 8) {
+			for (i = 8; i < twits.length; i++) {
+				if ((twits[i].childNodes[1].childNodes[0].textContent.includes(twitSearch.value)) || (twits[i].childNodes[1].childNodes[1].textContent.includes(twitSearch.value))) {
+					twits[i].classList.remove('hidden');
+					continue;
+				}
+				else {
+					twits[i].classList.add('hidden');
+				}
+			}
+	}
+}
+
+
 
 button.addEventListener('click', modalToggle);
 modalCancelButton.addEventListener('click', modalToggle);
 modalCloseButton.addEventListener('click', modalToggle);
 modalAcceptButton.addEventListener('click', addTwit);
+twitSearchButton.addEventListener('click', search);
+twitSearch.addEventListener('keyup', search);
